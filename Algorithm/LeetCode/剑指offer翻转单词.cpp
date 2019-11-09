ebfa80727d7862f11201416c1f1470b0b3578828
WeiSenhui <cs_shwei@163.com>
2019-11-09 14:35:54 +0800

    LaTeX

---
+++ Algorithm/LeetCode/剑指offer翻转单词.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        reverse(s.begin(), s.end());
+        
+        int n = s.size();
+        
+        // 找出来某个连续的一段——非常经典
+        for(int i = 0; i < n; i ++) {
+            int j = i;
+            while (j < n && s[j] != ' ') j ++;
+            reverse(s.begin() + i, s.begin() + j);
+            i = j; // i此时指向空格，后面会+1，找下一个连续段
+        }
+        
+        return s;
+    }
+};
--- Algorithm/LeetCode/力扣周赛/第161场.cpp
+++ Algorithm/LeetCode/力扣周赛/第161场.cpp
@@ -10,26 +10,30 @@ public:
         int x1 = 0, x2 = 0;
         int n = s1.size(), cnt = 0;
         for (int i = 0; i < n; i ++) {
-
-
             if (s1[i] != s2[i]) {
-                if (s1[i] == 'x') {
-                    x1 ++;
-                }
-                if (s2[i] == 'x') {
-                    x2 ++;
-                }
+                if (s1[i] == 'x') x1 ++;
+                if (s2[i] == 'x') x2 ++;
             }
         }
 
         if ((x1 - x2) & 1) return -1;
-        // cout << x1 << " "<< x2 << endl;
 
         if (x1 & 1 ) {
             if (max(x1, x2) == 1) return 2;
             else return (x1 + x2 - 1) / 2 + 2;
         }
+
         return (x1 + x2) / 2;
     }
 };
 
+if (j1 >= 1 && j1 <= n && j2 >= 1 && j2 <= n) {
+    int t = w[i1][j1];
+    if (i1 != i2) t += w[i2][j2];
+
+    int &x = f[k][i1][i2]; // 引用，给变量取个别名
+    x = max(x, f[k - 1][i1 - 1][i2 - 1] + t);
+    x = max(x, f[k - 1][i1 - 1][i2] + t);
+    x = max(x, f[k - 1][i1][i2 - 1] + t);
+    x = max(x, f[k - 1][i1][i2] + t);
+}
