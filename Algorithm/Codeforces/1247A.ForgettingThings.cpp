3791aba8288da08004358e74eaf4d48ac66caabf
WeiSenhui <cs_shwei@163.com>
2019-11-21 16:36:35 +0800

    a

---
+++ Algorithm/Codeforces/1247A.ForgettingThings.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    if (a == 9 && b == 1) cout << 9 << " " << 10 << endl;
+    else if (a - b != 0 && a - b != -1) cout << -1 <<endl;
+    else {
+        if(a == b) cout << 10 * a << " " << 10 * b + 1 << endl;
+        else {
+            cout << 10 * a + 9 << " " << 10 * b << endl;
+        }
+    }
+    return 0;
+}
