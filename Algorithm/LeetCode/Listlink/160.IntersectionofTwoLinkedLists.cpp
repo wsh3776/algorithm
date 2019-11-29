8c220e034bf878768dd8aee0b34fa7950bd0370b
WeiSenhui <cs_shwei@163.com>
2019-11-29 20:21:21 +0800

    复习

--- Algorithm/LeetCode/Listlink/160.IntersectionofTwoLinkedLists.cpp
+++ Algorithm/LeetCode/Listlink/160.IntersectionofTwoLinkedLists.cpp
@@ -1,3 +1,10 @@
+算法：
+1.Two pointers do not intersect 
+p and q both move a+b step, then turn into NULL.
+2.Two pointers intersect
+p adn q both move a+b+c step, then they will meet.
+
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
--- Algorithm/LeetCode/test.cpp
+++ Algorithm/LeetCode/test.cpp
@@ -1,33 +1,3 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
-
-class Solution {
-public:
-    ListNode* removeNthFromEnd(ListNode* head, int n) {
-        auto dummy = new ListNode(-1);
-        dummy->next = head;
-
-        auto first = dummy;
-        auto second = dummy;
-        while(n --) {
-            first = first->next;
-        }
-
-        while(first->next) {
-            first = firsr->next;
-            second = second->next;
-        }
-
-        second->next = second->next->next;
-
-        return dummy->next;
-    }
-};
-
-
+for (int i = 0; i < n; i ++) {
+    
+}
